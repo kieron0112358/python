00cda08867f16820cbe88ad63d00e6cf7dccdc93
Kieron <1582257837@qq.com>
2018-09-11 17:43:54 +0800

    Signed-off-by: Kieron <1582257837@qq.com>创建thread.py

---
+++ pycv/pycv/pycv/thread.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import threading
+import time
+import inspect
+import ctypes
+def f1(n):
+    for i in range(200):
+        print("run %d"%i)
+        time.sleep(1)
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+ 
+def stop_thread(thread):
+    _async_raise(thread.ident, SystemExit)
+t = threading.Thread(target=f1,args=(1,))
+t.start()
+time.sleep(3)
+stop_thread(t)
